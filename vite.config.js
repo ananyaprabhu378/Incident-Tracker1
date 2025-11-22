97518508c8dd5e2e390fc4670701ed01b7e9737c
ananyaprabhu378 <ananyaprabhu378@gmail.com>
2025-11-22 14:04:30 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Incident-Tracker1/', // <-- set repo name or hosting base path
+  base: '/Incident-Tracker-ai/', // <-- set repo name or hosting base path
   plugins: [react()]
 })
